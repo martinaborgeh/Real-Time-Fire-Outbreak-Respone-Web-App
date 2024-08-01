66bc52c81b7d341f4fb0e04112e4e77e1a995cf7
Martin Aborgeh <121477525+martinaborgeh@users.noreply.github.com>
2024-08-01 14:21:52 +0000

    .env cleaned

--- backend/fire_outbreak_project/fire_outbreak_project/settings.py
+++ backend/fire_outbreak_project/fire_outbreak_project/settings.py
@@ -232,7 +232,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 
 CHANNEL_LAYERS = {
     "default": {
