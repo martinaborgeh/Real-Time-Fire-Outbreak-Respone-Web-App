16a6517e5aac383ce3cb2290f81e0a501fe53606
Martin Aborgeh <121477525+martinaborgeh@users.noreply.github.com>
2024-08-15 23:29:57 +0000

    installed babel and added ghana shapefile

--- backend/fire_outbreak_project/fire_outbreak_app/load.py
+++ backend/fire_outbreak_project/fire_outbreak_app/load.py
@@ -2,27 +2,40 @@ from pathlib import Path
 from django.contrib.gis.utils import LayerMapping
 from .models import Roads
 
+# roads_mapping = {
+#     'u': 'u',
+#     'v': 'v',
+#     'key': 'key',
+#     'osmid': 'osmid',
+#     'name': 'name',
+#     'highway': 'highway',
+#     'oneway': 'oneway',
+#     'reversed': 'reversed',
+#     'length': 'length',
+#     'lanes': 'lanes',
+#     'ref': 'ref',
+#     'junction': 'junction',
+#     'bridge': 'bridge',
+#     'maxspeed': 'maxspeed',
+#     'service': 'service',
+#     'tunnel': 'tunnel',
+#     'access': 'access',
+#     'geom': 'LINESTRING',
+# }
+
 roads_mapping = {
-    'u': 'u',
-    'v': 'v',
-    'key': 'key',
-    'osmid': 'osmid',
+   
+    'osmid': 'osm_id',
     'name': 'name',
-    'highway': 'highway',
+    'highway': 'fclass',
     'oneway': 'oneway',
-    'reversed': 'reversed',
-    'length': 'length',
-    'lanes': 'lanes',
     'ref': 'ref',
-    'junction': 'junction',
     'bridge': 'bridge',
     'maxspeed': 'maxspeed',
-    'service': 'service',
     'tunnel': 'tunnel',
-    'access': 'access',
     'geom': 'LINESTRING',
 }
-road_shp = Path(__file__).resolve().parent / "road_data" / "clipped_road_network.shp"
+road_shp = Path(__file__).resolve().parent / "road_data" / "ghana_osm_road_free_updated.shp"
 
 
 
